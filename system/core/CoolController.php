33f32d6e6fffd0399ff1756a559bf3a567743d2c
intril <jj.comeback@gmail.com>
2016-04-25 15:25:30 +0800

    modify get client ip

--- system/core/CoolController.php
+++ system/core/CoolController.php
@@ -52,7 +52,8 @@ abstract class CoolController {
         if ( getenv( 'HTTP_CLIENT_IP' ) && strcasecmp( getenv( 'HTTP_CLIENT_IP' ), 'unknown' ) ) {
             return getenv( 'HTTP_CLIENT_IP' );
         } elseif ( getenv( 'HTTP_X_FORWARDED_FOR' ) && strcasecmp( getenv( 'HTTP_X_FORWARDED_FOR' ), 'unknown' ) ) {
-            return getenv( 'HTTP_X_FORWARDED_FOR' );
+            $arr = getenv ( 'HTTP_X_FORWARDED_FOR' );
+            return trim ( $arr [0] );
         } elseif ( getenv( 'REMOTE_ADDR' ) && strcasecmp( getenv( 'REMOTE_ADDR' ), 'unknown' ) ) {
             return getenv( 'REMOTE_ADDR' );
         } elseif ( isset( $_SERVER['REMOTE_ADDR'] ) && $_SERVER['REMOTE_ADDR'] && strcasecmp( $_SERVER['REMOTE_ADDR'], 'unknown' ) ) {
